9e1e87a7c95c7e81fb0418532cadb94d73148573
Aboobacker Sidheeque <sidhub41214@gmail.com>
2024-12-18 10:14:48 +0530

    updated

---
+++ Basic Window/basicwindow.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+# Create the main window
+window = tk.Tk()
+window.title("My window")
+window.geometry("400x300") #Set window width and height
+
+# Run the window
+window.mainloop()
